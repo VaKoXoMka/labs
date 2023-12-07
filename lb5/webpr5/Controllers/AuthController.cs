a8ce0bb1d75af489920421078dd556a897062e8e
VaKoXoMka <92250976+VaKoXoMka@users.noreply.github.com>
2023-12-07 08:15:35 +0200

    Add files via upload

---
+++ lb5/webpr5/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using webpr5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace webpr5.Controllers
+{
+    public class AuthController : Controller
+    {
+        private AuthService _authService;
+
+        public AuthController(AuthService authService) => _authService = authService;
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _authService.Logout();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel model)
+        {
+            var isAuthenticated = _authService.Login(model);
+
+            if (isAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+            ModelState.AddModelError(string.Empty, "not loggined");
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserModel model)
+        {
+            var result = _authService.Register(model);
+
+            if (result)
+                return RedirectToAction("Index", "Home");
+
+            return View(model);
+        }
+    }
+}
---
+++ lb5/webpr5/Controllers/Labs.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMvcApp.Controllers
+{
+    public class Labs : Controller
+    {
+
+        [Authorize]
+        public IActionResult Lb1()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public void Lb1(string input_file, string output_file)
+            => webLib.Lb1.Pr1(input_file, output_file);
+
+        [Authorize]
+        public IActionResult Pr2()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public void Lb2(string input_file, string output_file)
+            => webLib.Lb2.Pr2(input_file, output_file);
+
+        [Authorize]
+        public IActionResult Pr3()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public void Pr3(string input_file, string output_file)
+             => webLib.Lb3.Pr3(input_file, output_file);
+    }
+}
